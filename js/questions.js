b01991b2edeb0babb00afe4dad74a413400f914b
dtrinh888 <dtrinh888@gmail.com>
2015-09-28 23:50:19 -0700

    created questions array

---
+++ js/questions.js
@@ -0,0 +1,97 @@
+//questions to push into array
+  questions.push({
+    question: 'Who knocked out the Lakers in the 2006-2007 playoffs?',
+    selection: ['Phoenix Suns', 'Denver Nuggets', 'San Antonio Spurs', 'Boston Celtics'],
+    answer: 0
+  });
+  questions.push({
+    question: 'Who ousted Elgin Baylor’s 34.0 PPG to take the Scoring Title in 1962 by more than 10.0 PPG?',
+    selection: ['Jerry West', 'Wilt Chamberlain', 'Bill Russell', 'Oscar Robertson'],
+    answer: 1
+  });
+  questions.push({
+    question: 'What year did the Lakers record their string of 33 consecutive wins?',
+    selection: ['1990-1991', '1971-1972', '2000-2001', '1957-1958'],
+    answer: 1
+  });
+  questions.push({
+    question: 'Which Laker holds the franchise record for most steals in a single game?',
+    selection: ['Magic Johnson', 'Elgin Baylor', 'Kobe Bryant', 'Jerry West'],
+    answer: 1
+  });
+  questions.push({
+    question: 'Which Laker does not have his number retired?',
+    selection: ['Byron Scott', 'James Worthy', 'Elgin Baylor', 'Gail Goodrich'],
+    answer: 0
+  });
+  questions.push({
+    question: 'What year did James Worthy\'s number retired?',
+    selection: ['1995', '2000', '2003', '1999'],
+    answer: 0
+  });
+  questions.push({
+    question: 'What number did Gail Goodrich have retired',
+    selection: ['11', '22', '25', '30'],
+    answer: 2
+  });
+  questions.push({
+    question: 'What year was Wilt Chamberlain\'s number retired as a Laker?',
+    selection: ['1983', '1990', '1992', '1997'],
+    answer: 0
+  });
+  questions.push({
+    question: 'Which Laker holds the franchise record for most rebounds in a single quarter?',
+    selection: ['Elgin Baylor', 'Shaquille O\'Neal', 'Wilt Chamberlain', 'Andrew Bynum'],
+    answer: 2
+  });
+  questions.push({
+    question:'What team did Kobe Bryant outscore through 3 quarters in 2006?',
+    selection: ['Toronto Raptors', 'Dallas Mavericks', 'Phoenix Suns', 'Denver Nuggets'],
+    answer: 1
+  });
+  questions.push({
+    question: 'How many times have the Lakers placed 1st in their division?',
+    selection: ['22', '31', '29', '17'],
+    answer: 1
+  });
+  questions.push({
+    question:'',
+    selection: ['1961', '1962', '1960', '1959'],
+    answer: 1
+  });
+  questions.push({
+    question: 'Wilt Chamberlain holds the franchise single season record of how many RPG',
+    selection: ['14.7 RPG', '18.3 RPG', '21.1 RPG', '26.9 RPG'],
+    answer: 2
+  });
+  questions.push({
+    question: 'Who knocked the Lakers out of the 2002-2003 NBA playoffs?',
+    selection: ['Detroit Pistons', 'Minnesota Timberwolves', 'San Antonio Spurs', 'Houston Rockets'],
+    answer: 2
+  });
+  questions.push({
+    question: 'Which Laker holds the franchise record for most points scored in a single season?',
+    selection: ['Elgin Baylor', 'Magic Johnson', 'Kobe Bryant', 'Kareem Abdul-Jabbar'],
+    answer: 2 
+  });
+  questions.push({
+    question: 'Kareem Abdul-Jabbar holds the Lakers franchise record with ____ career blocks.',
+    selection: ['1,599 Career Blocks', '1,781 Career Blocks', '2,012 Career Blocks', '2,694 Career Blocks'],
+    answer: 3
+  });
+  questions.push({
+    question: 'Which player was NOT drafted by the Lakers?',
+    selection: ['Mark Madsen', 'Sasha Vujacic', 'Byron Scott', 'Elden Campbell'],
+    answer: 2
+  });
+  questions.push({
+    question: 'Which Laker set the record for most points scored (61) in a single NBA finals game?',
+    selection: ['Elgin Baylor', 'Jerry West', 'Kareem Abdul-Jabbar', 'Wilt Chamberlain'],
+    answer: 0
+  });
+  questions.push({
+    question: 'What year did the Lakers join the BAA?',
+    selection: ['1937', '1942', '1948', '1951'],
+    answer: 2
+  });
+
